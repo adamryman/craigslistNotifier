3c3b1ac171ac1560f83ecf7cb5e6422acfce66f6
Adam Ryman <adamryman@gmail.com>
2014-01-16 02:24:17 -0800

    Now writes and reads old data from file. Might go into database later

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import feedparser
 
 
 entries = {}
+entriesFile = open('entries.txt', 'r')
+
+for line in entriesFile:
+  entries[line.rstrip()] = True
+
+entriesFile.close()
+entriesFile = open('entries.txt', 'a')
+
 feed = feedparser.parse('http://seattle.craigslist.org/search/sss?catAbb=sss&query=expedit&sort=date&format=rss')
+print entries.keys()
 
 for i in range(0, len(feed['entries'])):
-  if feed['entries'][i].link not in entries:
-    print feed[entries][i].link
+  entrie = feed['entries'][i].link
+  if entrie not in entries:
+    entriesFile.write(entrie + '\n')
+    print entrie
 
-for i in range(0, len(feed['entries'])):
-  entries[feed['entries'][i].link] = feed['entries'][0]
+  entries[feed['entries'][i].link] = feed['entries'][i]
+
+entriesFile.close()
