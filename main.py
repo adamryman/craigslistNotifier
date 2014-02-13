143745c5fcdd06d0636568d66fe12b65eb6284f2
lelandbatey <lelandbatey@gmail.com>
2014-02-12 18:53:43 -0800

    Fixed bug which always sent email.
    There was a mistake in the previous version such that even if no new posts had been made, it would still send an email. This resulted in waaaaay to many emails.
    
    Additionally, switched the files from space-based indentation to tab-based indentation.

--- main.py
+++ main.py
@@ -32,130 +32,130 @@ import os
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def location(file):
-    return os.path.join(__location__,file)
+	return os.path.join(__location__,file)
 
 
 def checkAccountData(silent = True):
-    if not os.path.exists(location('accountData.txt')):
-        accountData = open(location('accountData.txt'), 'w')
-        if int(raw_input('Which way would you like to receive notifications?:\n\t[1] Twilio\n\t[2] Gmail\n')) == 2:
-            print "You've selected 'Gmail'"
-            accountData.write('GMAIL\n')
-            accountData.write(raw_input('Gmail user (user_name@gmail.com): ') + '\n')
-            accountData.write(raw_input('Gmail password: ') + '\n')
-        else:
-            print "You've selected 'Twilio'"
-            accountData.write('TWILIO\n')
-            accountData.write(raw_input('Twilio ACCOUNT_SID: ') + '\n')
-            accountData.write(raw_input('Twilio AUTH_TOKEN: ') + '\n')
-            accountData.write(raw_input('Twilio Phone Number (+19876543210): ') + '\n')
-            accountData.write(raw_input('Receiving Phone Number (+19876543210): ') + '\n')
-            accountData.close()
-    # This is so if this function is used else where it can give the user
-    # feedback if there's already account data. However, when it's usually
-    # being run silent will be set to false and nothing will be printed.
-    elif not silent:
-        print "There's already account information entered. If you'd like to \
+	if not os.path.exists(location('accountData.txt')):
+		accountData = open(location('accountData.txt'), 'w')
+		if int(raw_input('Which way would you like to receive notifications?:\n\t[1] Twilio\n\t[2] Gmail\n')) == 2:
+			print "You've selected 'Gmail'"
+			accountData.write('GMAIL\n')
+			accountData.write(raw_input('Gmail user (user_name@gmail.com): ') + '\n')
+			accountData.write(raw_input('Gmail password: ') + '\n')
+		else:
+			print "You've selected 'Twilio'"
+			accountData.write('TWILIO\n')
+			accountData.write(raw_input('Twilio ACCOUNT_SID: ') + '\n')
+			accountData.write(raw_input('Twilio AUTH_TOKEN: ') + '\n')
+			accountData.write(raw_input('Twilio Phone Number (+19876543210): ') + '\n')
+			accountData.write(raw_input('Receiving Phone Number (+19876543210): ') + '\n')
+			accountData.close()
+	# This is so if this function is used else where it can give the user
+	# feedback if there's already account data. However, when it's usually
+	# being run silent will be set to false and nothing will be printed.
+	elif not silent:
+		print "There's already account information entered. If you'd like to \
 re-enter account information, delete the 'accountData.txt' file and try again."
 
 def getAccountData():
-    accountData = {}
-    accountFile = open(location('accountData.txt'), 'r')
-    sendType = accountFile.readline().rstrip()
-    if sendType == "GMAIL":
-        accountData['sendMethod'] = 'GMAIL'
-        accountData['GMAIL_USER'] = accountFile.readline().rstrip()
-        accountData['GMAIL_PSWD'] = accountFile.readline().rstrip()
+	accountData = {}
+	accountFile = open(location('accountData.txt'), 'r')
+	sendType = accountFile.readline().rstrip()
+	if sendType == "GMAIL":
+		accountData['sendMethod'] = 'GMAIL'
+		accountData['GMAIL_USER'] = accountFile.readline().rstrip()
+		accountData['GMAIL_PSWD'] = accountFile.readline().rstrip()
 
-    elif sendType == "TWILIO":
-        accountData['sendMethod'] = "TWILIO"
-        accountData['ACCOUNT_SID'] = accountFile.readline().rstrip()
-        accountData['AUTH_TOKEN'] = accountFile.readline().rstrip()
-        accountData['phoneSender'] = accountFile.readline().rstrip()
-        accountData['phoneReciver'] = accountFile.readline().rstrip()
+	elif sendType == "TWILIO":
+		accountData['sendMethod'] = "TWILIO"
+		accountData['ACCOUNT_SID'] = accountFile.readline().rstrip()
+		accountData['AUTH_TOKEN'] = accountFile.readline().rstrip()
+		accountData['phoneSender'] = accountFile.readline().rstrip()
+		accountData['phoneReciver'] = accountFile.readline().rstrip()
 
-    return accountData
+	return accountData
 
 def sendEmail(feed, accountData):
-    # print feed
-    data = feed.formatEmail()
-    if data['body']:
-        FROM = accountData['GMAIL_USER']
-        TO = accountData['GMAIL_USER']
-        MESSAGE = MIMEMultipart('alternative')
-        MESSAGE['subject'] = data['subject']
-        MESSAGE['From'] = FROM
-        MESSAGE['To'] = TO
-        MESSAGE.preamble = data['body']
-        MESSAGE.attach(MIMEText(data['body'], 'html'))
-        # Attempt to send email
-        try:
-            server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
-            server.ehlo()
-            server.starttls()
-            server.login(accountData['GMAIL_USER'], accountData['GMAIL_PSWD'])
-            server.sendmail(FROM, TO, MESSAGE.as_string())
-            server.close()
-            print 'Successfully sent email'
-        except Exception as e:
-            print "Failed to send mail"
-            print e
+	# print feed
+	data = feed.formatEmail()
+	if data['body']:
+		FROM = accountData['GMAIL_USER']
+		TO = accountData['GMAIL_USER']
+		MESSAGE = MIMEMultipart('alternative')
+		MESSAGE['subject'] = data['subject']
+		MESSAGE['From'] = FROM
+		MESSAGE['To'] = TO
+		MESSAGE.preamble = data['body']
+		MESSAGE.attach(MIMEText(data['body'], 'html'))
+		# Attempt to send email
+		try:
+			server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+			server.ehlo()
+			server.starttls()
+			server.login(accountData['GMAIL_USER'], accountData['GMAIL_PSWD'])
+			server.sendmail(FROM, TO, MESSAGE.as_string())
+			server.close()
+			print 'Successfully sent email'
+		except Exception as e:
+			print "Failed to send mail"
+			print e
 
 
 def sendSMS(feed, accountData):
-    ACCOUNT_SID = accountData['ACCOUNT_SID']
-    AUTH_TOKEN = accountData['AUTH_TOKEN']
-    phoneSender = accountData['phoneSender']
-    phoneReciver = accountData['phoneReciver']
+	ACCOUNT_SID = accountData['ACCOUNT_SID']
+	AUTH_TOKEN = accountData['AUTH_TOKEN']
+	phoneSender = accountData['phoneSender']
+	phoneReciver = accountData['phoneReciver']
 
-    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)    
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)    
 
-    text = feed.formatSMS()
+	text = feed.formatSMS()
 
-    ##If the string is blank send it along
-    if text is not "":
-        client.messages.create(
-            from_= phoneSender, to = phoneReciver, body = text
-        )
+	##If the string is blank send it along
+	if text is not "":
+		client.messages.create(
+			from_= phoneSender, to = phoneReciver, body = text
+		)
 
 def writeOldEntries(feeds):
-    entriesFile = open(location('entries.txt'), 'w')
+	entriesFile = open(location('entries.txt'), 'w')
 
-    for feed in feeds:
-        entriesFile.write(feed.buildEntryLine())
-    entriesFile.close()
+	for feed in feeds:
+		entriesFile.write(feed.buildEntryLine())
+	entriesFile.close()
 
 
 
 def main():
-    ##Setting up variables
-    if(len(sys.argv) > 2):
-        rssfeed = sys.argv[1]+","+sys.argv[2]
-        open(location('rss.txt'), 'a').write(rssfeed+'\n')
+	##Setting up variables
+	if(len(sys.argv) > 2):
+		rssfeed = sys.argv[1]+","+sys.argv[2]
+		open(location('rss.txt'), 'a').write(rssfeed+'\n')
 
-    if not os.path.exists(location('rss.txt')):
-        print "Add an RSS feed using install.py or pass an RSS feed and a name for that feed as two separate argument; Exiting."
-        sys.exit()
+	if not os.path.exists(location('rss.txt')):
+		print "Add an RSS feed using install.py or pass an RSS feed and a name for that feed as two separate argument; Exiting."
+		sys.exit()
 
 
-    checkAccountData()
-    
-    transmitDict = {}
-    transmitDict['GMAIL'] = sendEmail
-    transmitDict['TWILIO'] = sendSMS
+	checkAccountData()
+	
+	transmitDict = {}
+	transmitDict['GMAIL'] = sendEmail
+	transmitDict['TWILIO'] = sendSMS
 
-    feeds = feedHandler.buildFeeds()
+	feeds = feedHandler.buildFeeds()
 
-    accountData = getAccountData()
-    # sendFunc is a function that we get out of the transmit dict. By doing
-    # this, it means not much code is required to add other ways of sending
-    # information.
-    sendFunc = transmitDict[accountData['sendMethod']]
+	accountData = getAccountData()
+	# sendFunc is a function that we get out of the transmit dict. By doing
+	# this, it means not much code is required to add other ways of sending
+	# information.
+	sendFunc = transmitDict[accountData['sendMethod']]
 
-    for x in feeds:
-        sendFunc(x, accountData)
+	for x in feeds:
+		sendFunc(x, accountData)
 
-    writeOldEntries(feeds)
+	writeOldEntries(feeds)
 
 
 
@@ -163,5 +163,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+	main()
 
